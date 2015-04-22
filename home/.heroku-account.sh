f09be10eba5ef25d679abbc16d58dcd84b2f4179
aleontiev <alonetiev@gmail.com>
2015-04-22 18:36:27 -0400

    add heroku-account.sh

---
+++ home/.heroku-account.sh
@@ -0,0 +1,71 @@
+export NETRC="$HOME/.netrc"
+
+function heroku-current() {
+    if [ -f "$NETRC" ]
+    then
+        echo "Current user:" $(cat $NETRC | grep login | uniq | sed -E s/login// | xargs)
+    else
+        echo "Could not find $NETRC, try running 'heroku-account login'"
+    fi
+}
+
+function heroku-login() {
+    if [ -f "$NETRC" ]
+    then
+        rm $NETRC
+    fi
+    heroku login
+    NAME=$1
+    if [ -z "$NAME" ]
+    then
+        cp $NETRC $NETRC.$USER
+    else
+        cp $NETRC $NETRC.$NAME
+    fi
+    heroku-current
+}
+
+function heroku-switch() {
+    TO_NETRC="$NETRC.$1"
+    if [ -f "$TO_NETRC" ] 
+    then
+        cp $TO_NETRC $NETRC
+        heroku-current
+    else
+        echo "Cannot find $TO_NETRC, try running 'heroku-account login $1'"
+    fi
+}
+
+function heroku-list() {
+    PREFIX=$HOME/.netrc.
+    for f in $PREFIX*
+    do
+        if [[ "$f" == *"*"* ]]
+        then
+            echo "Could not find anything like $NETRC.*, try running 'heroku-account login'"
+        else
+            SHORT_NAME=$(echo $f | sed s#$PREFIX## | xargs)
+            LOGIN_NAME=$(cat $f | grep login | uniq | sed -E s/login// | xargs)
+            echo $SHORT_NAME ":" $LOGIN_NAME
+        fi
+    done
+}
+
+function heroku-account() {
+    if [ -z "$1" ] 
+    then
+        heroku-current
+    else
+        if [ "$1" == "login" ]
+        then
+            heroku-login $2
+        else
+            if [ "$1" == "list" ]
+            then
+                heroku-list
+            else
+                heroku-switch "$1"
+            fi
+        fi
+    fi
+}
